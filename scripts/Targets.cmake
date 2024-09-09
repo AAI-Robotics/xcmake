d4c6a0566200bd900d6d42382a180061ff90a701
Nicholas Tomlinson <nstomlinson@gmail.com>
2024-09-09 19:59:11 +0100

    Make switch statements work in new Clang
    The -Wno-switch-default and -Wcovered-switch-default make it
    impossible to write a warning-free switch statement covering all
    the values of an enum. Since -Wcovered-switch-default lets us have
    a warning if we add an enum value but forget to update all the
    switch statements that are affected (except those that already did
    not cover all the cases individually), that warning is better.
    
    The -Wno-switch-default warning appears to be new in Clang 18.

--- scripts/Targets.cmake
+++ scripts/Targets.cmake
@@ -276,6 +276,7 @@ function(apply_default_standard_properties TARGET)
             -Wno-shadow-uncaptured-local         # A useful pattern is to std::move something into a lambda.
             -Wno-global-constructors             # We also use these
             -Wno-missing-prototypes
+            -Wno-switch-default                  # Incompatible with -Wcovered-switch-default, which is better.
             -Wno-switch-enum                     # This is stupid.
             -Wno-unused-template                 # ... We're writing a template library...
             -Wno-float-equal                     # This isn't always wrong...
